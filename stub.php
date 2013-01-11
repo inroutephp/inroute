bc1361087d4f57642257123861482e97f6ff4877
itbz <hannes.forsgard@gmail.com>
2013-01-11 20:44:58 +0100

    keep track of stub during initial development

---
+++ stub.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace itbz\inroute;
+
+include "vendor/autoload.php";
+
+/**
+ * @inroute
+ */
+class Controller
+{
+    /**
+     * @inject $a   someObjectFactory
+     * @inje ct $b someOtherFactory
+     * @inject     $c    yeah
+     */
+    public function __construct(SomeObject $a, $c)
+    {
+        $this->a = $a;
+    }
+
+    /**
+     * @route GET /domain/{:name}
+     */
+    public function view(Route $route)
+    {
+        var_dump($route);
+    }
+}
+
+// Jag håller på och skriver test för denna första del i ReflectionClassTest...
+
+$reflect = new ReflectionClass("itbz\inroute\Controller");
+
+header('Content-Type: text/plain');
+
+if ($reflect->isInroute()) {
+    print_r($reflect->getInjections());
+    print_r($reflect->getRoutes());
+}
+
+
+die();
+
+
+// @inject leder till kod i den här stilen
+class Dependencies
+{
+    private $container;
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+    function PlaskingView()
+    {
+        // Det här är ju inte helt Pimple. Fundera på vad jag vill kräva av världen...
+        $a = $this->container['someObjectFactory']();
+
+        // Detta test kan bli fel om projektet är i ett annat namespace
+        if (!$a instanceof SomeObject) {
+            $msg = 'DI-container method someObjectFactory must return a SomeObject instance.';
+            //throw new DependencyExpection($msg);
+            echo $msg;die();
+        }
+
+        // Detta kan också bli fel om projektet är i ett annat namespace
+        return new PlaskingView($a);
+    }
+}
+
+
+// bogus..
+class Route {}
+class SomeObject {}
+
+
+// Ska egentligen skapas med hjälp av plask.json i plasksite.php (gateway)
+/*
+    I en json-konfigurationsfil så ska
+        sökvägen till en fil som returnerar en Caller finnas..
+        samt sökvägen till en fil som returnerar en container..
+        om vad DIC returnerar ska kontrolleras ska vara en inställning
+
+    Dessa filer samt den autogenererade koden ska laddas i en gateway
+        plasksite.php (behöver inte läsa json, utan skapas från template...)
+
+        Det ska vara upp till användaren att skriva en index.php där gateway anropas
+            jag vet inte om användaren använder mod_rewrite eller någon annan lösning
+            jag vill att användaren ska ha ett bra ställa att kicka in logg med
+                exemplevis monolog. (Gärna med aspects!! Så tycker jag att logning
+                ska implementeras, se till att mitt projekt har bra tänk för detta!!)
+ */
+$wwwRoot = 'någon definierad root siten använder sig av..';
+$caller = new DefaultCaller;
+$container = array(
+    'someObjectFactory' => function(){
+        return new SomeObject;
+    }
+);
+$deps = new Dependencies($container);
+
+
+// @route skapar kod i den här stilen
+$actionTest = function() use ($deps, $caller) {
+    $view = $deps->PlaskingView();
+    
+    // Ska egentligen hämtas från min router...
+    // Jag vill skriva ett eget Route-object
+    //      så att jag kan byta mellan olika Routers utan
+    //      att användarkod behöver förändras...
+    $route = new Route;
+    $route->test = 'yeah';
+
+    $caller->call(array($view, 'view'), $route);
+};
+
+// kär min lilla test..
+$actionTest();
+
+// Om jag väljer Aura-router..
+$routes = array(
+    $wwwRoot . 'domain' => array(
+        'routes' => array(
+            array(
+                'path' => '/{:name}',
+                'method' => array('GET'),
+                'values' => array(
+                    'action' => $actionTest
+                )
+            )
+        )
+    )
+);
+
+/*
+
+    TODO:
+
+    1) Titta på olika routers och bestämma mig för en
+    2) Definiera Route-gränssnittet (och implementera för den router jag valt)
+    3) Hitta någon parser som kan parsa mina @inject och @route keywords (phpDocumentor ?)
+    4) Kontrollera så att allt fungerar med namespaces
+    5) Autogenerera kod med hjälp av enkla templates
+    6) Skriv plasksite.php
+    7) Baka ihop allt i en phar att användas i build-cykel
+
+
+    Hur ska koden användas i projekt?
+    =================================
+    
+    development:
+
+    $plask = new Plask('plask.json');
+    $plask->run($requestUri, $_SERVER);
+
+    production:
+
+    > php plask.phar build plask.json
+    $plask = include plasksite.php;
+    $plask->run($requestUri, $_SERVER);
+
+ */
