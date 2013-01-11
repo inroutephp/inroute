e3737961cc90e17b31445d0dee1d9dbddff643ca
itbz <hannes.forsgard@gmail.com>
2013-01-11 18:21:39 +0100

    Fixed year in copyright claim

--- src/itbz/inroute/DefaultCaller.php
+++ src/itbz/inroute/DefaultCaller.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the inroute package
  *
- * Copyright (c) 2012 Hannes Forsgård
+ * Copyright (c) 2013 Hannes Forsgård
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
