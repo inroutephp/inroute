df0dbe7bb9c44485f19556a675a2dfd924f8563a
itbz <hannes.forsgard@gmail.com>
2013-01-12 01:29:19 +0100

    Added the dependency exception

---
+++ src/itbz/inroute/Exception/DependencyExpection.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the inroute package
+ *
+ * Copyright (c) 2013 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\inroute
+ */
+
+namespace itbz\inroute\Exception;
+
+use itbz\inroute\Exception;
+
+/**
+ * Exception thrown when dependency generation fails
+ *
+ * @package itbz\inroute\Exception
+ */
+class DependencyExpection extends Exception
+{
+}
