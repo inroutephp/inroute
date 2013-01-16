1f66e836a0811f6df633e8bd6e05aaa6f03c23bc
itbz <hannes.forsgard@gmail.com>
2013-01-16 12:39:59 +0100

    Added test container

--- stub.php
+++ stub.php
@@ -5,26 +5,10 @@ namespace itbz\inroute;
 include "vendor/autoload.php";
 header('Content-Type: text/plain');
 
-class Cont extends \Pimple
-{
-    public function __construct()
-    {
-        $this['foobar'] = function ($c) {
-            return new \DateTime;
-        };
-        $this['xfactory'] = function ($c) {
-            return array();
-        };
-        $this['xx'] = function ($c) {
-            return 'xx';
-        };
-    }
-}
-
 $facade = new InrouteFacade(array(
     'root' => '',
     'caller' => 'DefaultCaller',
-    'container' => 'Cont',
+    'container' => '\itbz\test\Container',
     // Tre stycken som kan vara både array eller sträng...
     'prefixes' => array(
         'php'
---
+++ tests/itbz/test/Container.php
@@ -0,0 +1,18 @@
+<?php
+namespace itbz\test;
+
+class Container extends \Pimple
+{
+    public function __construct()
+    {
+        $this['foobar'] = function ($c) {
+            return new \DateTime;
+        };
+        $this['xfactory'] = function ($c) {
+            return array();
+        };
+        $this['xx'] = function ($c) {
+            return 'xx';
+        };
+    }
+}
