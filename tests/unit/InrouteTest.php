a2fb856bcb3baf1c5a67f88eef9c5e6def62e441
iio <hannes.forsgard@gmail.com>
2013-02-09 17:45:14 +0100

    Added copyright claim to unit tests

--- tests/unit/InrouteTest.php
+++ tests/unit/InrouteTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the inroute package
+ *
+ * Copyright (c) 2013 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace iio\inroute;
 
 class InrouteTest extends \PHPUnit_Framework_TestCase
