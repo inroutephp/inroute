1e59dbee36b8b09e53bca0060725894c880ca8d8
Hannes Forsgård <hannes.forsgard@fripost.org>
2019-12-23 23:12:21 +0100

    Add more type hints

--- src/Compiler/Psr4ClassFinder.php
+++ src/Compiler/Psr4ClassFinder.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 
 namespace inroutephp\inroute\Compiler;
 
+/**
+ * @implements \IteratorAggregate<string>
+ */
 final class Psr4ClassFinder implements \IteratorAggregate
 {
     /**
